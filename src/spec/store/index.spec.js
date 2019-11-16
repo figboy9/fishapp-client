c08cf7537f9ddfe4acf0a3f65dda6283df935df8
ezio1119 <52276064+ezio1119@users.noreply.github.com>
2019-11-16 19:49:03 +0900

    Update index.spec.js
    コメントアウトを無効化

--- src/spec/store/index.spec.js
+++ src/spec/store/index.spec.js
@@ -1,23 +1,23 @@
-// import Vuex from 'vuex'
-// import { createLocalVue } from '@vue/test-utils'
-// import cloneDeep from 'lodash.clonedeep'
-// import index from '../../store'
+import Vuex from 'vuex'
+import { createLocalVue } from '@vue/test-utils'
+import cloneDeep from 'lodash.clonedeep'
+import index from '../../store'
 
-// const localVue = createLocalVue()
-// localVue.use(Vuex)
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
-// describe('store/index.js', () => {
-//   let store
+describe('store/index.js', () => {
+  let store
 
-//   beforeEach(() => {
-//     store = new Vuex.Store(cloneDeep(index))
-//   })
+  beforeEach(() => {
+    store = new Vuex.Store(cloneDeep(index))
+  })
 
-//   describe('mutations', () => {
-//     test('setIsLoggedIn ミューテーションに true がコミットされると、isLoggedIn ステートの値が true になる', () => {
-//       expect(store.getters.isLoggedIn).toBe(false)
-//       store.commit('setIsLoggedIn', true)
-//       expect(store.getters.isLoggedIn).toBe(true)
-//     })
-//   })
-// })
+  describe('mutations', () => {
+    test('setIsLoggedIn ミューテーションに true がコミットされると、isLoggedIn ステートの値が true になる', () => {
+      expect(store.getters.isLoggedIn).toBe(false)
+      store.commit('setIsLoggedIn', true)
+      expect(store.getters.isLoggedIn).toBe(true)
+    })
+  })
+})
